3b8ae402afdb2c3fd670670bd3521da497eadf98
Mark Novak <mark.novak@oregonstate.edu>
2023-06-25 13:40:22 -0700

    Add files via upload

---
+++ test.r
@@ -0,0 +1,4 @@
+lambda <- 2  # this line sets the variable "lambda" equal to 2 TimeSteps <- 10 # sets the variable "TimeSteps" equal to 10 t <- c(1:TimeSteps) # creates a vector from 1 to the number of timesteps.  Could also use the seq(1,TimeSteps,1) command
+N <- numeric() # creates an empty numeric vector
+
+print(lambda)
